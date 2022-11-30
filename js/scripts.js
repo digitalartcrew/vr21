bca14a7f5b49461f50b11c8e15dcfff7e2b1af38
Donte Burney <donteburney@Dontes-Mac-mini.local>
2022-11-30 06:10:20 -1000

    added console statements

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+console.log("Are you really sure I am linked?");
+
+const myName = "Donte";
+console.log("My name: ", myName);
+
+const number = 9;
+console.log("Type of number: ", typeof number);
+
+const isLoggedIn = true;
+console.log("Is logged in: ", isLoggedIn);
