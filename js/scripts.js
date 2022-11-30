16d73f890bfdd794c0657fe385db69b7a651748a
Donte Burney <donteburney@Dontes-Mac-mini.local>
2022-11-30 12:57:48 -1000

    added pseudocode for calc UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
-console.log("Are you really sure I am linked?");
+const userAge = prompt("How old are you?");
 
-const myName = "Donte";
-console.log("My name: ", myName);
-
-const number = 9;
-console.log("Type of number: ", typeof number);
-
-const isLoggedIn = true;
-console.log("Is logged in: ", isLoggedIn);
+if (userAge > 21) {
+  console.log("You can enter the club");
+} else {
+  console.log("Go Home!");
+}
