f48918b5e137b139a909d9fefce2262099f345d1
Donte Burney <donteburney@Dontes-Mac-mini.local>
2022-12-08 13:32:24 -1000

    api

--- js/promise.js
+++ js/promise.js
@@ -1,11 +1,17 @@
-fetch("https://jsonplaceholder.typicode.com/photos")
-  .then((response) => response.json())
-  .then((json) => {
+const url =
+  "https://api.unsplash.com/search/photos?query=cars&per_page=20&client_id=nLM4Hq-rLb931DiT6toUYJdw6g2I-A9wGdZyc4i2YOI";
+
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
     const container = document.querySelector(".container");
 
-    for (let i = 0; i < json.length - 1; i++) {
+    for (let i = 0; i < data.results.length - 1; i++) {
       const img = document.createElement("img");
-      img.setAttribute("src", json[i].url);
+
+      img.setAttribute("src", data.results[i].urls.thumb);
 
       container.append(img);
     }
